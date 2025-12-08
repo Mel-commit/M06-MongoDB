8800478ba91dd5f29242d5e38bdc6588adf2f42c
mel-commit <chuber35@ivytech.edu>
2025-12-08 18:55:05 -0500

    added db

---
+++ FrontEnd/script.js
@@ -0,0 +1,13 @@
+//we are going to make an event listener... it will trigger with the DOM is loaded
+addEventListener("DOMContentLoaded", async function(){
+    const response = await this.fetch("http://localhost:3000/api/songs")
+    const songs = await response.json
+
+    let html = ""
+    for (let song of songs){
+        html+=`<li>${song.title} - ${song.artist}</li>`
+    }
+
+    this.document.querySelector("#addedsong").innerHTML = html
+    
+})
