7bd328881ea35f701345576e744d0257520ed6fa
mel-commit <chuber35@ivytech.edu>
2025-12-08 22:37:55 -0500

    followed tut

--- app.js
+++ app.js
@@ -23,7 +23,21 @@ router.get("/songs", async(req,res) =>{
 
 }) 
 
-router.post("/songs", async(req,res) => {
+//Grab a single song in the database
+router.get("/songs/:id", async (req,res) =>{
+    try{
+        const song = await Song.findById(req.params.id)
+        res.json(song)
+
+    }
+    catch (err){
+       res.status(404).send(err) 
+    }
+    
+})
+
+//added a song to the database
+router.post("/songs", async (req,res) => {
     try{
         const song = await new Song(req.body)
         await song.save()
@@ -33,6 +47,24 @@ router.post("/songs", async(req,res) => {
     catch(err){
         res.status(400).send(err)
     }
+})
+
+
+//update is to update an exisitng record/resource/database entry.. it uses a put request
+router.put("/songs/:/id", async(req,res) =>{
+   //first we need to find and update the song the front end wants us to update.
+   //to do this we need to request the id of the song from request
+   //and the find it in the database and update it
+   try {
+        const song = req.body
+        await Song.updateOne({_id: req.params.id},song)
+        console.log(song)
+        res.sendStatus(204)
+
+    }
+   catch(err){
+        res.status(400).send(err)
+   } 
 })
 
 
